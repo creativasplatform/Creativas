b3a911d7d274f26103fa4db74d1f6dab0d7d24fe
Alfredo <alfredocastellanoula.14@gmail.com>
2024-06-30 14:15:09 -0600

    Pathners

--- src/frontend/src/landing/Pathners.jsx
+++ src/frontend/src/landing/Pathners.jsx
@@ -10,32 +10,33 @@ const Pathners = () => {
                         Crowfund the projects <br />on multiples <br />networks
                     </h1>
                 </div>
-                <div className="flex items-center flex-wrap justify-around gap-12">
+                <div className="flex items-center flex-wrap justify-around gap-2">
                     <div className="flex flex-col items-center w-11">
                         <div className="relative w-20 h-20">
-                            <img alt="eth" fetchpriority="high" decoding="async" data-nimg="fill" style={{ position: 'absolute', height: '100%', width: '100%', left: 0, top: 0, right: 0, bottom: 0, color: 'transparent' }} src="https://1inch.io/img/main/eth.webp" />
+                            <img alt="eth" fetchpriority="high" decoding="async" data-nimg="fill" style={{ position: 'absolute', height: '100%', width: '100%', left: 0, top: 0, right: 0, bottom: 0, color: 'transparent' }} src="https://dev.rootstock.io/assets/img/rsk/RBTC-logo.png" />
+                         
                         </div>
-                        <p className="text-white text-opacity-60 text-base font-normal leading-7 mb-4">Etherum</p>
+                        <p className="text-white mt-6 text-opacity-60 text-base font-roboto leading-7 mb-4">Roostock</p>
                     </div>
                     <div className="flex flex-col items-center w-11">
                         <div className="relative w-20 h-20">
-                            <img alt="eth" fetchpriority="high" decoding="async" data-nimg="fill" style={{ position: 'absolute', height: '100%', width: '100%', left: 0, top: 0, right: 0, bottom: 0, color: 'transparent' }} src="https://1inch.io/img/main/bsc.webp" />
+                            <img alt="eth" fetchpriority="high" decoding="async" data-nimg="fill" style={{ position: 'absolute', height: '100%', width: '100%', left: 0, top: 0, right: 0, bottom: 0, color: 'transparent' }} src="https://cryptologos.cc/logos/internet-computer-icp-logo.svg?v=032" />
                         </div>
-                        <p className="text-white text-opacity-60 text-base font-normal leading-7 mb-4">BNB Chain</p>
+                        <p className="text-white mt-6 text-opacity-60 text-base font-roboto leading-7 mb-4">ICP</p>
                     </div>
                     <div className="flex flex-col items-center w-11">
                         <div className="relative w-20 h-20">
-                            <img alt="eth" fetchpriority="high" decoding="async" data-nimg="fill" style={{ position: 'absolute', height: '100%', width: '100%', left: 0, top: 0, right: 0, bottom: 0, color: 'transparent' }} src="https://1inch.io/img/main/polygon.webp" />
+                            <img alt="eth" fetchpriority="high" decoding="async" data-nimg="fill" style={{ position: 'absolute', height: '100%', width: '100%', left: 0, top: 0, right: 0, bottom: 0, color: 'transparent' }} src="https://cryptologos.cc/logos/polygon-matic-logo.png" />
                         </div>
-                        <p className="text-white text-opacity-60 text-base font-normal leading-7 mb-4">Polygon</p>
+                        <p className="text-white mt-6 ml-8 text-opacity-60 text-base font-roboto leading-7 mb-4">Polygon</p>
                     </div>
+
                     <div className="flex flex-col items-center w-11">
                         <div className="relative w-20 h-20">
-                            <img alt="eth" fetchpriority="high" decoding="async" data-nimg="fill" style={{ position: 'absolute', height: '100%', width: '100%', left: 0, top: 0, right: 0, bottom: 0, color: 'transparent' }} src="https://1inch.io/img/main/polygon.webp" />
+                            <img alt="eth" fetchpriority="high" decoding="async" data-nimg="fill" style={{ position: 'absolute', height: '100%', width: '100%', left: 0, top: 0, right: 0, bottom: 0, color: 'transparent' }} src="https://cryptologos.cc/logos/versions/ethereum-eth-logo-diamond-purple.svg?v=032" />
                         </div>
-                        <p className="text-white text-opacity-60 text-base font-normal leading-7 mb-4">Etherum</p>
+                        <p className="text-white mt-6 text-opacity-60 text-base font-roboto leading-7 mb-4">Etherum</p>
                     </div>
-                 
                 </div>
             </div>
         </section>
