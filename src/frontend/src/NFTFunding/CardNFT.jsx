e66674d1dfc799ff97a9f2d0c38869a851456df3
Alfredo <alfredocastellanoula.14@gmail.com>
2024-07-03 15:05:08 -0600

    Scripts

--- src/frontend/src/NFTFunding/CardNFT.jsx
+++ src/frontend/src/NFTFunding/CardNFT.jsx
@@ -1,10 +1,9 @@
 import React from 'react';
-import Prueba from '../assets/Prueba.png';
 
-const Card = ({ title, description, image }) => {
+const CardNFT = ({ title, description, image, className, style }) => {
   return (
-<div className="bg-gradient-to-c-custom text-white rounded-xl shadow-lg w-96 h-72">
-      <img src={Prueba} alt={title} className="w-[360px] mt-2 h-48 object-cover rounded-xl" />
+    <div className={`bg-gradient-to-c-custom text-white rounded-xl shadow-lg w-80 h-72 mb-4 ${className}`} style={style}>
+      <img src={image} alt={title} className="w-[300px] mt-2 h-48 object-cover rounded-xl" />
       <div className="p-4">
         <h3 className="text-lg font-bold">{title}</h3>
         <p className="text-sm">{description}</p>
@@ -13,4 +12,4 @@ const Card = ({ title, description, image }) => {
   );
 };
 
-export default Card;
+export default CardNFT;
--- src/frontend/src/NFTFunding/Categories.jsx
+++ src/frontend/src/NFTFunding/Categories.jsx
@@ -1,54 +1,29 @@
 import React from 'react';
-import Card from './CardNFT';
+import CardNFT from './CardNFT';
 import CategoriesBar from './CategoriesBar';
 import masicon from "../assets/mas.png"; // Importa el icono aquí
 import useAssets from '../hooks/Nftventure/useAssets';
-const Categories = ({onOpenModal}) => {
-  const { startedAssets } = useAssets()
-  const collections = [
-    {
-      title: 'Tecate Comuna',
-      description: '9.2K items - 10.8K ETH',
-    },
-    {
-      title: 'Tecate Comuna',
-      description: '9.2K items - 10.8K ETH',
-    },
-    
-    {
-      title: 'Tecate Comuna',
-      description: '9.2K items - 10.8K ETH',
-    },
-    
-    {
-      title: 'Tecate Comuna',
-      description: '9.2K items - 10.8K ETH',
-    },
-        {
-      title: 'Tecate Comuna',
-      description: '9.2K items - 10.8K ETH',
-    },
-    
-    
+import { Card, Skeleton } from "@nextui-org/react";
 
-
-    // Otros elementos de colección pueden ir aquí si los tienes
-  ];
+const Categories = ({ onOpenModal }) => {
+  const { startedAssets, loadingStarted, errorStarted } = useAssets();
+  const collections = startedAssets.map(asset => ({
+    title: asset.title,
+    description: `${asset.price} ETH`,
+    image: asset.mainPhoto,
+  }));
 
   return (
     <div className="bg-[#0b0c0c] text-white p-4">
       <div className="flex items-center justify-between">
-        {/* Título "Explore collections" a la izquierda con tipografía semibold */}
         <h2 className="text-5xl font-semibold mb-4 mt-8 ml-8">Explore Projects</h2>
-
-        {/* Botón "Create a Project" con el icono al lado y tipografía thin */}
         <div className="flex items-center">
           <button
-          onClick={onOpenModal}
+            onClick={onOpenModal}
             type="button"
-            className="mr-28 text-white bg-secondary hover:bg-secondary-ligth focus:outline-none font-thin rounded-full text-lg px-5 py-2.5 text-center md:text-left dark:bg-secondary dark:hover:bg-secondary-ligth  dark:focus:ring-blue-800 flex items-center"
+            className="mr-28 text-white bg-secondary hover:bg-secondary-ligth focus:outline-none font-thin rounded-full text-lg px-5 py-2.5 text-center md:text-left dark:bg-secondary dark:hover:bg-secondary-ligth dark:focus:ring-blue-800 flex items-center"
           >
-            <img src={masicon} className="h-4 w-4 mr-2" alt="Add Icon" /> {/* Añade margen a la derecha del icono */}
+            <img src={masicon} className="h-4 w-4 mr-2" alt="Add Icon" /> 
             Create a Project
           </button>
         </div>
@@ -57,14 +32,40 @@ const Categories = ({onOpenModal}) => {
       <CategoriesBar />
       
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 mt-8">
-        {collections.map((collection, index) => (
-          <Card
-            key={index}
-            title={collection.title}
-            description={collection.description}
-            image={collection.image}
-          />
-        ))}
+        {loadingStarted ? (
+          // Renderizar Skeletons mientras los datos están cargando
+          Array.from({ length: 24 }).map((_, index) => (
+            <Card key={index} className="w-[300px] h-[250px] space-y-5 p-4" radius="lg">
+              <Skeleton className="rounded-lg">
+                <div className="h-24 rounded-lg bg-default-300"></div>
+              </Skeleton>
+              <div className="space-y-3">
+                <Skeleton className="w-3/5 rounded-lg">
+                  <div className="h-3 w-3/5 rounded-lg bg-default-200"></div>
+                </Skeleton>
+                <Skeleton className="w-4/5 rounded-lg">
+                  <div className="h-3 w-4/5 rounded-lg bg-default-200"></div>
+                </Skeleton>
+                <Skeleton className="w-2/5 rounded-lg">  
+                  <div className="h-3 w-2/5 rounded-lg bg-default-300"></div>
+                </Skeleton>
+              </div>
+            </Card>
+          ))
+        ) : errorStarted ? (
+          <p>Error loading projects</p>
+        ) : (
+          collections.map((collection, index) => (
+            <CardNFT
+              key={index}
+              title={collection.title}
+              description={collection.description}
+              image={collection.image}
+              className="transition-opacity duration-500"
+              style={{ opacity: loadingStarted ? 0 : 1 }}
+            />
+          ))
+        )}
       </div>
     </div>
   );
