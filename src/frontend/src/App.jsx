c261d4fd7dfb81fa7f65b2f3d7bd2cb74b3aada1
danielaestefania19 <danielacastellano1000@gmail.com>
2024-07-13 23:26:35 -0600

    Cart Details NFT

--- src/frontend/src/App.jsx
+++ src/frontend/src/App.jsx
@@ -1,9 +1,12 @@
 // src/App.jsx
 import React from "react";
 import { Routes, Route } from 'react-router-dom';
-import BodyMain from "./landing/BodyMain.jsx"
+import Body from "./landing/Body.jsx";
+import Navbar from "./landing/Navbar.jsx";
 import Card from "./landing/Card.jsx";
+import NavbarMarketplace from "./NFTFunding/NavbarMarketplace.jsx";
 import NFTFunding from "./NFTFunding/NFTFunding.jsx";
+import Categories from "./NFTFunding/Categories.jsx";
 import { UserProvider } from './context/userContext';
 import { DfinityProvider } from "./context/IdentityContext.jsx";
 import Footer from "./landing/Footer.jsx";
@@ -13,33 +16,31 @@ import NotableDrops from "./landing/NotableDrops.jsx";
 import HeroSection from "./landing/HeroSection.jsx";
 import PromoBanner from "./landing/PromoBanner.jsx";
 import Numbers from "./landing/Numbers.jsx";
-import Patherns from "./landing/Pathners.jsx";
+import Pathners from "./landing/Pathners.jsx";
 import NFTs from "./landing/NFTs..jsx";
 import Contact from "./landing/Contact.jsx";
 import Video from "./landing/Video.jsx";
+import NFTDetail from "./NFTFunding/NFTDetails/Details.jsx"; // Importa el nuevo componente
+
 function App() {
   return (
     <div className="App">
-
       <UserProvider>
         <DfinityProvider>
           <Routes>
             <Route path="/" element={
               <>
-                <BodyMain />
-                <HeroSection/>
-                <PromoBanner/>
-                <Numbers/>
-      <div className="absolute top-4/4 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-rgba-104-207-256 blur-40 opacity-100 w-64 h-64 rounded-lg"></div>
-
+                <Navbar />
+                <Body />
+                <HeroSection />
+                <PromoBanner />
+                <Numbers />
+                <div className="absolute top-4/4 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-rgba-104-207-256 blur-40 opacity-100 w-64 h-64 rounded-lg"></div>
+                <NotableDrops />
                 <NFTSteps />
-                <Video/>
-                <Patherns />
+                <Video />
+                <Pathners />
                 <Card />
-
-
-
-
                 <Carousel />
                 <NFTs />
                 <Contact />
@@ -50,13 +51,18 @@ function App() {
               <>
                 <NFTFunding />
                 <Footer />
-
+              </>
+            } />
+            <Route path="/nft/:id" element={
+              <>
+                <Navbar />
+                <NFTDetail />
+                <Footer />
               </>
             } />
           </Routes>
         </DfinityProvider>
       </UserProvider>
-
     </div>
   );
 }
